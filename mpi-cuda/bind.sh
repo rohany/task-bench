30fb61d12dea560bded7cab49fcd4f6d6ca38b32
root <root@eos0077.eos.clusters.nvidia.com>
2024-12-30 17:06:43 -0800

    mpi-cuda: update to binding script

--- mpi-cuda/bind.sh
+++ mpi-cuda/bind.sh
@@ -2,11 +2,11 @@
 export CUDA_VISIBLE_DEVICES=$OMPI_COMM_WORLD_LOCAL_RANK
 
 # Use for profiling?
-export PMIX_MCA_gds=hash
-if [ $OMPI_COMM_WORLD_RANK -eq 1 ]; then
-  nsys profile -t mpi,cuda,osrt "$@"
-else
-  "$@"
-fi
+# export PMIX_MCA_gds=hash
+# if [ $OMPI_COMM_WORLD_RANK -eq 1 ]; then
+#   nsys profile -t mpi,cuda,osrt "$@"
+# else
+#   "$@"
+# fi
 
 $@
