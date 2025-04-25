f4af4c6c39b83b3d2d6378439f6d8be7b95d444f
Rohan Yadav <rohany@g0003.stanford.edu>
2025-04-24 22:24:30 -0700

    realm: switch some instances to RDMA memory

--- realm/main.cc
+++ realm/main.cc
@@ -1027,7 +1027,7 @@ void top_level_task(const void *args, size_t arglen, const void *userdata,
 
       for (long proc_index = 0; proc_index < num_procs; ++proc_index) {
         auto proc = procs.at(proc_index);
-        auto memory = proc_sysmems.at(proc);
+        auto memory = proc_regmems.at(proc);
 
         long first_point = proc_index * graph.max_width / num_procs;
         long last_point = (proc_index + 1) * graph.max_width / num_procs - 1;
@@ -1069,7 +1069,7 @@ void top_level_task(const void *args, size_t arglen, const void *userdata,
     args.num_procs = num_procs;
     args.num_fields = num_fields;
     args.force_copies = force_copies;
-    args.sysmem = proc_sysmems[proc];
+    args.sysmem = proc_regmems[proc];
     args.regmem = proc_regmems[proc];
     args.sync = sync_bar;
     args.first_start = first_start_bar;
